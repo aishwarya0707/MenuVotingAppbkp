6360949a0ae62de9a67e1b030a86451967958f98
aishwarya3431 <aishwarya3431@gmail.com>
2024-09-14 01:26:31 +0530

    Modified serializer

--- menu_voting_app/restaurants/serializers.py
+++ menu_voting_app/restaurants/serializers.py
@@ -33,37 +33,3 @@ class VoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vote
         fields = ["menu", "employee", "points"]
-
-
-class VoteRequestSerializer(serializers.Serializer):
-    """
-    Serializer for handling vote requests, including validation for multiple menu votes.
-    """
-
-    employee_id = serializers.PrimaryKeyRelatedField(
-        queryset=Employee.objects.all()
-    )  # Validate employee ID against Employee model.
-
-    menu_1 = serializers.PrimaryKeyRelatedField(
-        queryset=Menu.objects.all()
-    )  # Validate that menu_1 is a valid Menu instance.
-    menu_2 = serializers.PrimaryKeyRelatedField(
-        queryset=Menu.objects.all()
-    )  # Validate that menu_2 is a valid Menu instance.
-    menu_3 = serializers.PrimaryKeyRelatedField(
-        queryset=Menu.objects.all()
-    )  # Validate that menu_3 is a valid Menu instance.
-
-    def validate(self, data):
-        """
-        Custom validation for ensuring that all menu choices are unique.
-        """
-        menu_1, menu_2, menu_3 = data["menu_1"], data["menu_2"], data["menu_3"]
-
-        # Ensure all menu choices are unique
-        if len({menu_1, menu_2, menu_3}) != 3:
-            raise serializers.ValidationError(
-                "Menus must be unique."
-            )  # Raise an error if duplicate menus are found.
-
-        return data
